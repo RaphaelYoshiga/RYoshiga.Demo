960e35551b4dc4bda302ea9da194c377b3a51e1b
Raphael Yoshiga <raphael.yoshiga@hotmail.com>
2020-03-10 22:14:44 +0000

    Weather forecast, add extra property to test CI/CD

--- RYoshiga.Demo.WebApi/Controllers/WeatherForecastController.cs
+++ RYoshiga.Demo.WebApi/Controllers/WeatherForecastController.cs
@@ -31,7 +31,8 @@ namespace RYoshiga.Demo.WebApi.Controllers
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
+                Summary = Summaries[rng.Next(Summaries.Length)],
+                AccuracyPercentage = 0
             })
             .ToArray();
         }
--- RYoshiga.Demo.WebApi/WeatherForecast.cs
+++ RYoshiga.Demo.WebApi/WeatherForecast.cs
@@ -11,5 +11,6 @@ namespace RYoshiga.Demo.WebApi
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string Summary { get; set; }
+        public decimal AccuracyPercentage { get; set; }
     }
 }
