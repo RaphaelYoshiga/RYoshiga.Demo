7d928eb11a6590c3feab230d6739fd31dfc0b38f
Raphael Yoshiga <raphael.yoshiga@hotmail.com>
2020-03-15 10:22:00 +0000

    Add unit tests and try to run them on CI

---
+++ RYoshiga.Demo.UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace RYoshiga.Demo.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
